fa6f7720e0ab96256138c4ef642c4fedd18b8f3a
yoru <juanitplays@gmail.com>
2019-05-05 19:02:51 -0300

    mapa bugado

--- animal/lib/pages/home.dart
+++ animal/lib/pages/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:titled_navigation_bar/titled_navigation_bar.dart';
-
+import 'package:animal/pages/Map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key, this.user}) : super(key: key);
@@ -12,44 +13,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Widget> containers = [
+    Container(
+      color: Colors.pink,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.deepPurple,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home ${widget.user.email}'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home ${widget.user.email}'),
+        ),
+        
+        bottomNavigationBar: TitledBottomNavigationBar(
+          reverse: true,
+            onTap: (index) {
+              if (index == 1) {
+                 Navigator.push(context,MaterialPageRoute(builder: (context) => MapSample(), fullscreenDialog: true));
+              }
+              print("Selected Index: $index");
+            },
+            items:[
+              TitledNavigationBarItem(title: 'Home', icon: Icons.home),
+              TitledNavigationBarItem(title: 'Search', icon: Icons.pets,),
+              TitledNavigationBarItem(title: 'Profile', icon: Icons.person_outline),
+            ]),
 
-
+          //body:,
+          //  body: Title(color: null, child: null),
+//        body: TabBarView(children: <Widget>[
+//          Center(child: Text("nossa")),
+//          Center(child: Text("nosssa")),
+//          Center(child: Text("nossssa")),
+//        ]),
       ),
-
-bottomNavigationBar: TitledBottomNavigationBar(
-  onTap: (index){
-    print("Selected Index: $index");
-  },
-  items: [
-      TitledNavigationBarItem(title: 'Home', icon: Icons.home),
-      TitledNavigationBarItem(title: 'Search', icon: Icons.search),
-      TitledNavigationBarItem(title: 'Bag', icon: Icons.card_travel),
-      TitledNavigationBarItem(title: 'Orders', icon: Icons.shopping_cart),
-      TitledNavigationBarItem(title: 'Profile', icon: Icons.person_outline),
-  ]
-),
-
-      body: Form(child: Column(
-          children: <Widget>[
-//            TextField(
-//              autofocus: true,
-//            ),
-//            TextField(
-//              autofocus: true,
-//            ),
-//      RaisedButton(
-//
-//      child: Text('Publicar'),
-//      ),
-
-      ],
-    ),)
-    ,
     );
   }
 }
+
+
